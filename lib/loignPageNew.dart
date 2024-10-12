85e1540cf67928fc1526e34a17393f2727ecffe1
mousa <07711909037musa@gmail.com>
2024-10-12 18:44:48 +0300

    update page login change clolr red

--- lib/loignPageNew.dart
+++ lib/loignPageNew.dart
@@ -7,7 +7,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.red,
       appBar:AppBar(),
         body:Column(
           mainAxisAlignment: MainAxisAlignment.start,
